667876bfde3bdcb35c5550b4ba01629de3bc27af
Andy <735188273@qq.com>
2018-01-03 18:42:44 +0800

    p4

--- js/main.js
+++ js/main.js
@@ -5,13 +5,32 @@ $.fn.extend({
     if (typeof option.time !== "number") {
       option.time = 900;
     }
-    setTimeout(function () {
-    $(this).removeClass(option.method);
-    },1000)
+    setTimeout(function() {
+      $(this).removeClass(option.method);
+    }, 1000);
     setTimeout(function() {
       option.callBack.call(option.context);
     }, option.time);
     return $(this);
+  },
+  playAnimation: function(innerTime, callBack) {
+    if (typeof innerTime !== "number") innerTime = 800;
+    var time;
+    $.each(this, function(indexInArray, valueOfElement) {
+      var offset = 0;
+      if (typeof valueOfElement.offset === "number") {
+        offset = valueOfElement.offset;
+      }
+      time = indexInArray * innerTime + offset;
+      setTimeout(function() {
+        $(valueOfElement.ele).css(valueOfElement.css);
+      }, time);
+    });
+    console.log(time);
+
+    setTimeout(function() {
+      callBack.call(this);
+    }, time);
   }
 });
 
@@ -97,24 +116,40 @@ function p2Init() {
     // trigger:"click",
     easing: "easeInQuint"
   });
-  $('.p2 .next').click(function(e){
-    $('.p2').animateCss({
-      method:"fadeOut",
-      callBack:function(){
-        $('.p2').addClass('hidden');
+  $(".p2 .next").click(function(e) {
+    $(".p2").animateCss({
+      method: "fadeOut",
+      callBack: function() {
+        $(".p2").addClass("hidden");
         $(".p3").removeClass("hidden");
-        setTimeout(function(){
+
+        setTimeout(function() {
           p3Init();
-        },800);
+        }, 800);
       },
-      context:this
-    })
+      context: this
+    });
   });
 }
 
 //p3
 function p3Init() {
-  $('.p3 .next').removeClass('hidden');
+  $(".p3 .next").removeClass("hidden");
+  $(".p3 .next").click(function() {
+    $(".p3").animateCss({
+      method: "fadeOut",
+      callBack: function() {
+        $(".p3").addClass("hidden");
+        $(".p4").removeClass("hidden");
+        setTimeout(function() {
+          p4Init();
+        }, 800);
+      },
+      context: this
+    });
+  });
+  $('.p3 .title').removeClass('hidden');
+
   $(".p3 .car-1-wrapper").slide({
     mainCell: ".bd ul",
     autoPlay: true,
@@ -141,19 +176,67 @@ function p3Init() {
     duration: 1500,
     easing: "easeInQuad",
     elasticity: 0,
-    autoPlay:false,
+    autoPlay: false
   });
 }
 
 //p4
-// function p4Init(){
-//   var p4=anime.timeline()
-//   p4.add({targets:})
-// }
+function p4Init() {
+  var parent = ".p4 ";
+  var animationList = [
+    {
+      ele: parent + ".bus",
+      css: {
+        bottom: "41.5%"
+      }
+    },
+    {
+      ele: parent + ".car-1",
+      css: {
+        bottom: "42.03%"
+      }
+    },
+    {
+      ele: parent + ".car-2",
+      css: {
+        bottom: "43.49%"
+      },
+      offset: -500
+    },
+    {
+      ele: parent + ".car-3",
+      css: {
+        bottom: "52.87%"
+      },
+      offset: -500 * 2
+    },
+    {
+      ele: parent + ".car-4",
+      css: {
+        bottom: "52.8%"
+      },
+      offset: -500 * 3
+    },
+    {
+      ele: parent + ".car-5",
+      css: {},
+      offset: -500 * 4
+    },
+    {
+      ele: parent + ".car-6",
+      css: {},
+      offset: -500 * 5
+    }
+  ];
+  $.each(animationList, function(indexInArray, valueOfElement) {
+    valueOfElement.css.transform = "scale(1)";
+  });
+  $(animationList).playAnimation(800, function() {
+    $(".p4 .next").removeClass("hidden");
+  });
+}
 
-
-
-// p1Init();
-// setTimeout(function(){
-//   p3Init();
-// },1000)
+p1Init();
+// setTimeout(function() {
+//   p4Init();
+// }, 1000);
